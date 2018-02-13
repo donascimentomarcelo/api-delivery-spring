8e0e1fe7edb612792cfd00229c7ddae23d419d39
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-02-13 18:18:26 -0200

    Somente @JsonIgnore

--- src/main/java/br/com/cursomc/domain/Categoria.java
+++ src/main/java/br/com/cursomc/domain/Categoria.java
@@ -11,8 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Categoria implements Serializable{	
 	private static final long serialVersionUID = 1L;
@@ -24,7 +22,7 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
+
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
