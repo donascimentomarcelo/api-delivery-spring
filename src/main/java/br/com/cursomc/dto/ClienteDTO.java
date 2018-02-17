6e05c68e4efdef0988726225f6ef5d0a9fee770d
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-02-17 00:36:17 -0200

    Validacao personalizada de email unico na atualizacao do cliente

--- src/main/java/br/com/cursomc/dto/ClienteDTO.java
+++ src/main/java/br/com/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import br.com.cursomc.domain.Cliente;
+import br.com.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
