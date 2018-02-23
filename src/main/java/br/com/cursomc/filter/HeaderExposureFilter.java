447e98f5e59d29a0ea00b17b0482c472fe3d9179
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-02-23 12:58:54 -0300

    Expondo header location nas respostas

---
+++ src/main/java/br/com/cursomc/filter/HeaderExposureFilter.java
@@ -0,0 +1,39 @@
+package br.com.cursomc.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HeaderExposureFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.addHeader("access-control-expose-headers", "location");
+		chain.doFilter(req, res);
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
